180615593dd488297f318c05648d06e721fd61cd
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-09-03 03:09:04 -0400

    feat: aot (#1)
    Co-authored-by: Igor Alentev <mattephi@Igors-MacBook-Pro.local>

--- test.py
+++ test.py
@@ -1,30 +1,34 @@
-# from codegen import evaluate_f
 
-# print(evaluate_f([2.0]))
-
-import jax
 import jax.numpy as jnp
+import casadi as ca
+from jaxadi import compile, declare, lower
 
-nnz_in = [1]
-nnz_out = [1]
-n_w = 1
+# Example Usage:
+# Define a CasADi function with known inputs
+x = ca.SX.sym('x', 10)
+y = ca.SX.sym('y', 10)
+casadi_function = ca.Function('cf', [x, y], [x**2 + y**2 + 1])
 
+# Define a corresponding JAX fn str
+jax_fn = """
+def jax_function(x, y):
+    print('cached')
+    return x**2 + y**2 + 1"""
+jax_function = declare(jax_fn)
 
-@jax.jit
-def evaluate_f(inputs):
-    # Initialize the buffer array
-    work = jnp.zeros(1)
+# To hlo function
+hlo_jax = lower(jax_function, casadi_function)
+print(hlo_jax.as_text())
 
-    # Apply operations in sequence
-    work = (
-        work.at[0]
-        .set(inputs[0])  # Set initial value
-        .at[0]
-        .set(work[0] * work[0])  # Square the value
-        # Additional arbitrary operations can be chained here
-    )
-    return work
+# Cache and compile the JAX function
+compiled_jax_function = compile(jax_function, casadi_function)
 
+# Now you can use compiled_jax_function with actual JAX arrays
+x_jax = jnp.ones(10, dtype=jnp.float32)
+y_jax = jnp.ones(10, dtype=jnp.float32)
+print('Call after compilation')
+result = compiled_jax_function(x_jax, y_jax)
+result = compiled_jax_function(x_jax, y_jax)
+result = compiled_jax_function(x_jax, y_jax)
 
-result = evaluate_f([2.0])
-print(result)  # Should print [4.0]
+print(result)
